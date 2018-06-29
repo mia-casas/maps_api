8f09f92acac79dc9bbffce35c6de70f4449406cb
Mia Casas <miacasas@mias-mbp.fcnt.franklincollege.edu>
2018-06-28 23:11:17 -0400

    working

--- googleMaps.js
+++ googleMaps.js
@@ -1,31 +1,29 @@
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position){
-        var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-        var myOptions = {
-            zoom: 8,
-            center: latlng,
-            mapTypeId: google.maps.MapTypeId.TERRAIN,
-            disableDefaultUI: true,
-            zoomControl: true,
-            mapTypeControl: true,
-            scaleControl: true,
-            rotateControl: true,
-            streetViewControl: true,
-        }
-        var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-        var marker = new google.maps.Marker({
-            position: latlng,
-            map: map
-        });
-        
-        let contentString = '<div id="content"><h2 id="firstHeading" class="firstHeading">Custom info window</h2><p>This is a cool custom info window.</p></div>'
-        let infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
-        marker.addListener('click', function(){
-            infowindow.open(map, marker);
-        })
-        // var places = new google.maps.places.SearchBoxOptions
+if("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+    var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+    var myOptions = {
+      zoom: 8,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.TERRAIN,
+      disableDefaultUI: true,
+      zoomControl: true,
+      mapTypeControl: true,
+      scaleControl: true,
+      rotateControl: true,
+      streetViewControl: true,
+    }
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    var marker = new google.maps.Marker({
+    position: latlng,
+    map: map
+});
+let contentString = '<div id="content"><h2 id="firstHeading" class="firstHeading">Custom info window</h2><p>This is a cool custom info window.</p></div>';
+let infowindow = new google.maps.InfoWindow({
+    content: contentString
+});
+ marker.addListener('click', function() {
+    infowindow.open(map, marker);
+});
     });
 } else {
     var para = document.createElement('p');
